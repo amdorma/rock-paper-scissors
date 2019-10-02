bfa897af4747c5414e30dba39c239b749ddc06b0
Matt Malin <matt@amdorma.com>
2019-10-02 16:05:45 -0700

    Most HTML and CSS in place, JS is in the works

--- js/main.js
+++ js/main.js
@@ -1,55 +1,67 @@
-    function computerPlay(){
-        let compSelect = [
-        'rock',
-        'paper',
-        'scissors'];
-        let randomNumber = Math.floor(Math.random()*compSelect.length);
-        return compSelect[randomNumber];
+let buttons = document.querySelectorAll('button');
+let gameOver = false;
+let playerScore = 0
+let computerScore = 0;
+let gameWinner = '';
+
+function computerPlay(){
+    let compSelect = [
+    'rock',
+    'paper',
+    'scissors'];
+    let randomNumber = Math.floor(Math.random()*compSelect.length);
+    return compSelect[randomNumber];
+}
+
+function playRound(playerSelection, computerSelection) {
+    let playerWin = `You won the round! ${playerSelection} beats ${computerSelection}`;
+    let computerWin = `You lost the round! ${computerSelection} beats ${playerSelection}`;
+    let tie = `It\'s a tie! ${playerSelection} is the same as ${computerSelection}`;
+    if(playerSelection === computerSelection) {
+        return tie;
+    } else if( (playerSelection === 'rock' && computerSelection === 'paper') ||
+    (playerSelection === 'paper' && computerSelection === 'scissors') ||
+    (playerSelection === 'scissors' && computerSelection === 'rock') ){
+        return playerWin;
     }
-
-    function playerPlay(){
-        return playerSelect = prompt('Please choose from rock, paper, or scissors','').toLowerCase();
+     else {
+        return computerWin;
     }
+}
 
-    function playRound(playerChoice, compChoice) {
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const playerSelection = button.id;
+        const computerSelection = computerPlay();
+        if (gameOver === true) return;
+        playRound(playerSelection, computerSelection)
+    });
+});
 
-        compChoice = computerPlay();
-        playerChoice = playerPlay();
-        let playerWin = `You won the round! ${playerChoice} beats ${compChoice}`;
-        let computerWin = `You lost the round! ${compChoice} beats ${playerChoice}`;
-        let tie = `It\'s a tie! ${playerChoice} is the same as ${compChoice}`;
-        if(playerChoice === compChoice) {
-            return tie;
-        } else if( (playerChoice === 'rock' && compChoice === 'paper') ||
-        (playerChoice === 'paper' && compChoice === 'scissors') ||
-        (playerChoice === 'scissors' && compChoice === 'rock') ){
-            return computerWin;
-        }
-         else {
-            return playerWin;
-        }
-    }
 
-    function game() {
-        let playerScore = 0;
-        let computerScore = 0;
-        let tie = 0
-        for( round = 0; round < 5; round++ ){
-            let round = playRound();
-            if(round.indexOf('won') > -1) {
-                playerScore++
-            } else if (round.indexOf('lost') > -1){
-                computerScore++
-            } else {
-                tie++
-            }
-            console.log(`Player:${playerScore} vs Computer:${computerScore} Ties = ${tie}`);
-        }
-        let playerWinGame = `Player has won the game with ${playerScore} points!`;
-        let computerWinGame = `Computer has won the game with ${computerScore} points! Sucks to suck.`
-        if(playerScore > computerScore) {
-            return playerWinGame;
+/* console.log(playRound())
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let tie = 0
+    for( round = 0; round < 5; round++ ){
+        let round = playRound();
+        if(round.indexOf('won') > -1) {
+            playerScore++
+        } else if (round.indexOf('lost') > -1){
+            computerScore++
         } else {
-            return computerWinGame;
+            tie++
         }
+        console.log(`Player:${playerScore} vs Computer:${computerScore} Ties = ${tie}`);
     }
+    let playerWinGame = `Player has won the game with ${playerScore} points!`;
+    let computerWinGame = `Computer has won the game with ${computerScore} points! Sucks to suck.`
+    if(playerScore > computerScore) {
+        return playerWinGame;
+    } else {
+        return computerWinGame;
+    }
+}
+*/
