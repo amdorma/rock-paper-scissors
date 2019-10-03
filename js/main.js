d637b343d3a9d4e0f0faacc193266639ad457cf5
Matt Malin <matt@amdorma.com>
2019-10-03 12:54:20 -0700

    Finished JavaScript

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 let buttons = document.querySelectorAll('button');
-let gameOver = false;
-let playerScore = 0
-let computerScore = 0;
-let gameWinner = '';
+let playerScore = document.querySelector('#player-score');
+let computerScore = document.querySelector('#computer-score');
+let winner = document.querySelector('#status');
+let buttonSection = document.querySelector('.buttons')
+let results = document.querySelector('.results');
+let playAgain = document.createElement('button');
+playAgain.setAttribute('id', 'play-again')
+
+playAgain.textContent = 'Play again?';
+
+playAgain.addEventListener('click', () => {
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+    winner.textContent = 'First to five wins! Select your weapon!';
+    results.removeChild(playAgain);
+    buttons.forEach((button) => button.style.display = 'initial');
+})
 
 function computerPlay(){
     let compSelect = [
@@ -13,28 +26,44 @@ function computerPlay(){
     return compSelect[randomNumber];
 }
 
+function addPoint(score) {
+    let scoreCard = Number(score.textContent);
+    score.textContent = ++scoreCard;
+}
+
+function handleResult() {
+    const playerScoreFinal = Number(playerScore.textContent);
+    const computerScoreFinal = Number(computerScore.textContent);
+    if (playerScoreFinal == 5 || computerScoreFinal == 5) {
+        if (playerScoreFinal > computerScoreFinal) {
+            winner.textContent = "You Won!"
+        } else {
+            winner.textContent = "You lost!"
+        }
+        results.appendChild(playAgain);
+        buttons.forEach((button) => button.style.display = 'none');
+    }
+}
+
 function playRound(playerSelection, computerSelection) {
-    let playerWin = `You won the round! ${playerSelection} beats ${computerSelection}`;
-    let computerWin = `You lost the round! ${computerSelection} beats ${playerSelection}`;
-    let tie = `It\'s a tie! ${playerSelection} is the same as ${computerSelection}`;
     if(playerSelection === computerSelection) {
-        return tie;
+        return console.log('It\'s a tie!');
     } else if( (playerSelection === 'rock' && computerSelection === 'paper') ||
     (playerSelection === 'paper' && computerSelection === 'scissors') ||
     (playerSelection === 'scissors' && computerSelection === 'rock') ){
-        return playerWin;
+        addPoint(playerScore);
     }
      else {
-        return computerWin;
+        addPoint(computerScore);
     }
+    handleResult();
 }
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         const playerSelection = button.id;
         const computerSelection = computerPlay();
-        if (gameOver === true) return;
-        playRound(playerSelection, computerSelection)
+        playRound(playerSelection, computerSelection);
     });
 });
 
